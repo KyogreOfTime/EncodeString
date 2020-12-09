7d01d8409f526e4ea0c9b6381972a92578b20f6e
KyogreOfTime <75351912+KyogreOfTime@users.noreply.github.com>
2020-12-08 20:44:37 -0500

    Initial commit

---
+++ EncodeString/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace EncodeString
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncodeButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.InstructionLabel = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EncodeButton
+            // 
+            this.EncodeButton.Location = new System.Drawing.Point(107, 122);
+            this.EncodeButton.Name = "EncodeButton";
+            this.EncodeButton.Size = new System.Drawing.Size(94, 29);
+            this.EncodeButton.TabIndex = 0;
+            this.EncodeButton.Text = "Encode";
+            this.EncodeButton.UseVisualStyleBackColor = true;
+            this.EncodeButton.Click += new System.EventHandler(this.EncodeButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(168, 73);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(143, 27);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // InstructionLabel
+            // 
+            this.InstructionLabel.AutoSize = true;
+            this.InstructionLabel.Location = new System.Drawing.Point(40, 73);
+            this.InstructionLabel.Name = "InstructionLabel";
+            this.InstructionLabel.Size = new System.Drawing.Size(108, 20);
+            this.InstructionLabel.TabIndex = 2;
+            this.InstructionLabel.Text = "Enter text here:";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(181, 180);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 20);
+            this.ResultLabel.TabIndex = 3;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(54, 276);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(94, 29);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(168, 276);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(94, 29);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(14, 180);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(148, 20);
+            this.DescriptionLabel.TabIndex = 6;
+            this.DescriptionLabel.Text = "The resulting code is:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 332);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.InstructionLabel);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.EncodeButton);
+            this.Name = "Form1";
+            this.Text = "Encode String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncodeButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label InstructionLabel;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label DescriptionLabel;
+    }
+}
+
---
+++ EncodeString/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncodeString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EncodeButton_Click(object sender, EventArgs e)
+        {
+
+            //grabs user's input from InputTextBox
+            string userInput = InputTextBox.Text;
+
+            //changes capital letters to lowercase so the program isn't case-sensitive
+            string userInLower = userInput.ToLower();
+
+            //clears ResultLabel for subsequent clicks
+            ResultLabel.Text = "";
+                     
+            for(int index = 0; index < userInLower.Length; index++)
+            {
+                //checks string userInLower for repeated characters
+                if (userInLower.LastIndexOf(userInLower[index]) == userInLower.IndexOf(userInLower[index]))
+                {
+                    //Appends "(" to ResultLabel for every repeated char
+                    ResultLabel.Text += "(";
+                }
+                else
+                {
+                    //Appends ")" to ResultLabel for every non-repeated char
+                    ResultLabel.Text += ")";
+                }
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            //clears both InputTextBox and ResultLable
+            InputTextBox.Text = "";
+            ResultLabel.Text = "";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {            
+            //closes the application
+            this.Close();
+        }
+    }
+}
